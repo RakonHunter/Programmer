9d7657c28992b2498d83a4a786de02cdecf2a49a
RakonHugo <example@gmail.com>
2019-08-09 20:46:31 +0300

    test v2

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// let answer = prompt("18 maybe?", "Yes");
+
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+    
+incr ++;
+decr --;
+console.log(incr);    
+console.log(decr);    
